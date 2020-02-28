72d6200ad5df8402cf4f9516e5016b699b28dc1c
Ian Torres <iantorres@outlook.com>
2020-02-28 00:08:18 -0300

    Fix store methods similar code.

--- app/Http/Controllers/Api/Pets/Photos/StoreController.php
+++ app/Http/Controllers/Api/Pets/Photos/StoreController.php
@@ -5,6 +5,7 @@ namespace App\Http\Controllers\Api\Pets\Photos;
 use App\Http\Controllers\Controller;
 use App\Http\Requests\Api\Pets\Photos\StoreRequest;
 use App\Pet;
+use App\Repositories\LocationRepository;
 use App\Repositories\PhotoRepository;
 use Grimzy\LaravelMysqlSpatial\Types\Point;
 use Illuminate\Http\JsonResponse;
@@ -20,30 +21,13 @@ class StoreController extends Controller
      */
     public function __invoke(StoreRequest $request, Pet $pet)
     {
-        // TODO - Remove unnecessary complexity
-        $photo = PhotoRepository::createPhoto(
-            $request->user('api'),
-            $request->file('photo'),
-            $request->input('title'),
-            $request->input('body'),
-            $request->input('is_public'),
-            $request->input('coordinates')
-        );
-
-        $exploded = explode(',', $request->input('coordinates'));
-
+        $photo = PhotoRepository::createPhotoViaRequest($request);
+        $pet = LocationRepository::updateModelLocation($request, $pet);
         $pet->update([
             'photo_id' => $photo->id,
-            'location' => (new Point(
-                floatval($exploded[0]),
-                floatval($exploded[1])
-            )),
         ]);
-
         $photo->pets()->attach($pet->id);
-
         $photo->load('user', 'comment');
-
         return response()->json(
             $photo,
             200
--- app/Http/Controllers/Api/Users/Photos/StoreController.php
+++ app/Http/Controllers/Api/Users/Photos/StoreController.php
@@ -4,6 +4,7 @@ namespace App\Http\Controllers\Api\Users\Photos;
 
 use App\Http\Controllers\Controller;
 use App\Http\Requests\Api\Users\Photos\StoreRequest;
+use App\Repositories\LocationRepository;
 use App\Repositories\PhotoRepository;
 use App\User;
 use Grimzy\LaravelMysqlSpatial\Types\Point;
@@ -20,30 +21,10 @@ class StoreController extends Controller
      */
     public function __invoke(StoreRequest $request, User $user)
     {
-        // TODO - Remove unnecessary complexity
-        $photo = PhotoRepository::createPhoto(
-            $request->user('api'),
-            $request->file('photo'),
-            $request->input('title'),
-            $request->input('body'),
-            $request->input('is_public'),
-            $request->input('coordinates')
-        );
-
-        // TODO - Refactor this repeated code
-        $exploded = explode(',', $request->input('coordinates'));
-
-        $user->update([
-            'location' => (new Point(
-                floatval($exploded[0]),
-                floatval($exploded[1])
-            )),
-        ]);
-
+        $photo = PhotoRepository::createPhotoViaRequest($request);
+        $user = LocationRepository::updateModelLocation($request, $user);
         $photo->users()->attach($user->id);
-
         $photo->load('user', 'comment');
-
         return response()->json(
             $photo,
             200
