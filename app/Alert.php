5c378930aae2f63e41865281c0625f1f93fa647a
Ian Torres <iantorres@outlook.com>
2020-02-28 23:45:02 -0300

    Fill base model attributes and method.

--- app/Alert.php
+++ app/Alert.php
@@ -6,5 +6,41 @@ use Illuminate\Database\Eloquent\Model;
 
 class Alert extends Model
 {
-    //
+    /**
+     * The table name of alert.
+     *
+     * @var string
+     */
+    protected $table = 'alerts';
+
+    /**
+     * The mass assignment fields of alert.
+     *
+     * @var array
+     */
+    protected $fillable = [
+        'uuid',
+        'user_id',
+        'title',
+        'body',
+    ];
+
+    /**
+     * The hidden fields of alert.
+     *
+     * @var array
+     */
+    protected $hidden = [
+        'id'
+    ];
+
+    /**
+     * Get the route key for the alert.
+     *
+     * @return string
+     */
+    public function getRouteKeyName()
+    {
+        return 'uuid';
+    }
 }
