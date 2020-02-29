c08a264a3a8e3df7f0e7734c0791fc25e3c861b1
Ian Torres <iantorres@outlook.com>
2020-02-28 23:42:36 -0300

    Alert model and table added.

---
+++ database/migrations/2020_02_29_022950_create_alerts_table.php
@@ -0,0 +1,38 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateAlertsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('alerts', function (Blueprint $table) {
+            $table->bigIncrements('id');
+            $table->string('uuid')->index()->nullable();
+            $table->bigInteger('user_id')->unsigned()->index();
+            $table->morphs('alert');
+            $table->tinyInteger('type');
+            $table->point('location'); // Latitude, Longitude
+            $table->string('title');
+            $table->longText('body');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('alerts');
+    }
+}
