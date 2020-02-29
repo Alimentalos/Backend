1d2e71acae9787501ea03c018fd589e998c375f0
Ian Torres <iantorres@outlook.com>
2020-02-29 01:13:54 -0300

    Index controller modified to support ordering by latests and filter by status.

--- app/Http/Controllers/Api/Alerts/IndexController.php
+++ app/Http/Controllers/Api/Alerts/IndexController.php
@@ -5,6 +5,7 @@ namespace App\Http\Controllers\Api\Alerts;
 use App\Alert;
 use App\Http\Controllers\Controller;
 use App\Http\Requests\Api\Alerts\IndexRequest;
+use App\Repositories\StatusRepository;
 use Illuminate\Http\JsonResponse;
 
 class IndexController extends Controller
@@ -17,7 +18,12 @@ class IndexController extends Controller
      */
     public function __invoke(IndexRequest $request)
     {
-        $alerts = Alert::query()->paginate(25);
+        $alerts = Alert::query()
+            ->whereIn(
+                'status',
+                $request->has('whereInStatus') ?
+                    explode(',', $request->input('whereInStatus')) : StatusRepository::availableAlertStatuses() // Filter by statuses
+            )->latest('created_at')->paginate(25); // Order by latest
         return response()->json($alerts, 200);
     }
 }
