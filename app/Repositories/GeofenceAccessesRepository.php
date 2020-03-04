798b0346a7c13f34e1b5bf0214f7c497ccbab171
Ian Torres <iantorres@outlook.com>
2020-03-04 02:52:37 -0300

    Number of lines reduced on Geofence Resource AccessesController (Closes #54).

---
+++ app/Repositories/GeofenceAccessesRepository.php
@@ -0,0 +1,28 @@
+<?php
+
+
+namespace App\Repositories;
+
+use App\Geofence;
+use Illuminate\Contracts\Pagination\LengthAwarePaginator;
+use Illuminate\Http\Request;
+
+class GeofenceAccessesRepository
+{
+    /**
+     * Fetch via Geofence resource accesses.
+     *
+     * @param Request $request
+     * @param Geofence $geofence
+     * @param $resource
+     * @return LengthAwarePaginator
+     */
+    public static function fetchResourceViaRequest(Request $request, Geofence $geofence, $resource)
+    {
+        return $geofence->accesses()->with('accessible', 'first_location', 'last_location', 'geofence')
+            ->where('accessible_type', get_class(binder()::bindResourceModelClass($resource)))
+            ->where('geofence_uuid', $geofence->uuid)
+            ->latest()
+            ->paginate(20);
+    }
+}
