c46dade6cac1365c0bfbb4306400e334a44ff482
edidk7 <d.ignacio292@gmail.com>
2020-03-04 01:29:35 -0300

    Tests improved.

--- tests/Feature/Api/UserTest.php
+++ tests/Feature/Api/UserTest.php
@@ -17,6 +17,40 @@ class UserTest extends TestCase
     use RefreshDatabase;
 
     /**
+     * @test testOwnerUserWatchingSpecificUser
+     */
+    final public function testOwnerUserWatchingSpecificUser()
+    {
+        $user = factory(User::class)->create();
+        $userB = factory(User::class)->create();
+        $userB->is_public = false;
+        $userB->user_uuid = $user->uuid;
+        $userB->save();
+        $response = $this->actingAs($user, 'api')->json('GET', '/api/users/' . $userB->uuid);
+        $response->assertOk();
+        $response->assertJsonStructure([
+            'photo_url',
+            'email',
+            'name',
+            'is_public',
+            'location' => [
+                'type',
+                'coordinates'
+            ],
+            'uuid',
+            'updated_at',
+            'created_at',
+            'love_reacter_id',
+            'love_reactant_id',
+            'is_admin',
+            'is_child'
+        ]);
+        $response->assertJsonFragment([
+            'name' => $userB->name,
+            'email' => $userB->email,
+        ]);
+    }
+    /**
      * @test testUserWatchingOwner
      */
     final public function testUserWatchingOwner()
@@ -539,21 +573,6 @@ class UserTest extends TestCase
             ]);
 
         $response->assertStatus(403);
-    }
-
-    /**
-     * @test testOwnerUserWatchingSpecificUser
-     */
-    public function testOwnerUserWatchingSpecificUser()
-    {
-        // TODO - Add response structure and data asserts
-        $user = factory(User::class)->create();
-        $userB = factory(User::class)->create();
-        $userB->is_public = false;
-        $userB->user_uuid = $user->uuid;
-        $userB->save();
-        $response = $this->actingAs($user, 'api')->json('GET', '/api/users/' . $userB->uuid);
-        $response->assertOk();
     }
 
     /**
