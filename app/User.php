842cca3fd2238ff4e29bf6410a947358b7696d29
Ian Torres <iantorres@outlook.com>
2020-03-04 02:48:27 -0300

    Api token is now hidden (Closes #63).

--- app/User.php
+++ app/User.php
@@ -86,6 +86,7 @@ class User extends Authenticatable implements MustVerifyEmail, ReacterableContra
         'id',
         'password',
         'remember_token',
+        'api_token',
     ];
 
     /**
