e92ed7b6f47361082b931993c765f1eadd195907
Ian Torres <iantorres@outlook.com>
2020-03-04 03:00:48 -0300

    Number of lines removed on Resource Photos StoreController (Closes #60).

--- app/Http/Controllers/Api/Resource/Photos/StoreController.php
+++ app/Http/Controllers/Api/Resource/Photos/StoreController.php
@@ -4,8 +4,7 @@ namespace App\Http\Controllers\Api\Resource\Photos;
 
 use App\Http\Controllers\Controller;
 use App\Http\Requests\Api\Resource\Photos\StoreRequest;
-use App\Repositories\ModelLocationsRepository;
-use App\Repositories\PhotoRepository;
+use App\Repositories\ResourcePhotosRepository;
 use Illuminate\Http\JsonResponse;
 
 class StoreController extends Controller
@@ -17,15 +16,6 @@ class StoreController extends Controller
      */
     public function __invoke(StoreRequest $request, $resource)
     {
-        // TODO - Reduce number of lines of Resource Photos StoreController
-        // @body move into repository method as fetchViaRequest.
-        $photo = PhotoRepository::createPhotoViaRequest($request);
-        $resource = ModelLocationsRepository::updateModelLocation($request, $resource);
-        $resource->photos()->attach($photo->uuid);
-        $photo = $photo->load('comment');
-        return response()->json(
-            $photo,
-            200
-        );
+        return response()->json(ResourcePhotosRepository::createPhotoViaRequest($request, $resource),200);
     }
 }
