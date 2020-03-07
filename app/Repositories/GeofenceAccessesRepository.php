8b5d676ce82fb3c03eb040708a0b1281af9b6e2d
Ian Torres <iantorres@outlook.com>
2020-03-07 01:57:12 -0300

    Optimized imports.

--- app/Repositories/GeofenceAccessesRepository.php
+++ app/Repositories/GeofenceAccessesRepository.php
@@ -3,10 +3,7 @@
 
 namespace App\Repositories;
 
-use App\Contracts\Resource;
-use App\Geofence;
 use App\Lists\GeofenceAccessesList;
-use Illuminate\Contracts\Pagination\LengthAwarePaginator;
 
 class GeofenceAccessesRepository
 {
