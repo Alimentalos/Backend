2b0a9f553b9fcb9f4252904edaa551d81d418863
Ian Torres <iantorres@outlook.com>
2020-03-08 23:08:35 -0300

    Bumped to 2.0

--- app/Annotations/Base.php
+++ app/Annotations/Base.php
@@ -2,7 +2,7 @@
 
 /**
  * @OA\Info(
- *      version="v.1.3",
+ *      version="v.2.0",
  *      title="Alimentalos API",
  *      description="Official Alimentalos API Documentation",
  *      @OA\Contact(
