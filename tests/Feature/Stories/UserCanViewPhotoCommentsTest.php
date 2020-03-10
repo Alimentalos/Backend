f96e6b7d49418d87d3fe6a90161e79fca79c3bc8
Ian Torres <iantorres@outlook.com>
2020-03-10 22:19:59 +0000

    Test isolated.

---
+++ tests/Feature/Stories/UserCanViewPhotoCommentsTest.php
@@ -0,0 +1,86 @@
+<?php
+
+
+namespace Tests\Feature\Stories;
+
+
+use App\Comment;
+use App\Pet;
+use App\Photo;
+use App\User;
+use Illuminate\Foundation\Testing\DatabaseMigrations;
+use Tests\TestCase;
+
+class UserCanViewPhotoCommentsTest extends TestCase
+{
+    use DatabaseMigrations;
+
+    /**
+     * @test testIndexPhotosCommentsApi
+     */
+    final public function testIndexPhotosCommentsApi()
+    {
+        $user = factory(User::class)->create();
+        $pet = factory(Pet::class)->create();
+        $photo = factory(Photo::class)->create();
+        $photo->pets()->attach($pet);
+        $photo->save();
+        $comment = factory(Comment::class)->make();
+        $photo->comments()->create([
+            'user_uuid' => $user->uuid,
+            'body' => $comment->body,
+        ]);
+
+        $response = $this->actingAs($user, 'api')->json('GET', '/api/photos/' . $photo->uuid . '/comments');
+        $response->assertOk();
+
+        $response->assertJsonStructure([
+            'current_page',
+            'data' => [[
+                'uuid',
+                'user_uuid',
+                'title',
+                'body',
+                'commentable_type',
+                'commentable_id',
+                'created_at',
+                'updated_at',
+                'love_reactant_id',
+                'user' =>[
+                    'uuid',
+                    'user_uuid',
+                    'photo_uuid',
+                    'name',
+                    'email',
+                    'email_verified_at',
+                    'free',
+                    'photo_url',
+                    'location' =>[
+                        'type',
+                        'coordinates',
+                    ],
+                    'is_public',
+                    'created_at',
+                    'updated_at',
+                    'love_reactant_id',
+                    'love_reacter_id',
+                    'is_admin',
+                    'is_child',
+                ],
+            ]],
+            'first_page_url',
+            'from',
+            'last_page',
+            'last_page_url',
+            'next_page_url',
+            'path',
+            'per_page',
+            'prev_page_url',
+            'to',
+            'total'
+        ]);
+        $response->assertJsonFragment([
+            'user_uuid' => $user->uuid
+        ]);
+    }
+}
