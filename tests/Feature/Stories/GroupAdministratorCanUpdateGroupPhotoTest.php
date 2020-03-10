205e2e66efdd475c88960a58b33a09d4667d4fb4
Ian Torres <iantorres@outlook.com>
2020-03-10 14:17:12 +0000

    Email removed from data.

--- tests/Feature/Stories/GroupAdministratorCanUpdateGroupPhotoTest.php
+++ tests/Feature/Stories/GroupAdministratorCanUpdateGroupPhotoTest.php
@@ -59,8 +59,6 @@ class GroupAdministratorCanUpdateGroupPhotoTest extends TestCase
                 'user_uuid',
                 'photo_uuid',
                 'name',
-                'email',
-                'email_verified_at',
                 'free',
                 'photo_url',
                 'location' => [
