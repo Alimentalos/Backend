d557d609c4396a8cf744b012fdb7b0d2c4a1ea88
Ian Torres <iantorres@outlook.com>
2020-05-05 00:33:56 -0400

    Sentry added to track/handle exceptions also geo routes added manually on api routes file

--- app/Exceptions/Handler.php
+++ app/Exceptions/Handler.php
@@ -39,6 +39,9 @@ class Handler extends ExceptionHandler
      */
     public function report(Exception $exception)
     {
+        if (app()->bound('sentry') && $this->shouldReport($exception)) {
+            app('sentry')->captureException($exception);
+        }
         parent::report($exception);
     }
 
