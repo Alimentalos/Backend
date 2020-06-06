a61d2c2d40aac3b02fd24bff987636a93271b1a9
Ian Torres <iantorres@outlook.com>
2020-06-06 15:13:09 -0400

    Now you can create devices without marker and colors.

---
+++ tests/Feature/Stories/UserCanCreateDevicesWithoutColorsTest.php
@@ -0,0 +1,44 @@
+<?php
+
+
+namespace Tests\Feature\Stories;
+
+
+use Alimentalos\Relationships\Models\Device;
+use Alimentalos\Relationships\Models\User;
+use Illuminate\Foundation\Testing\RefreshDatabase;
+use Illuminate\Http\UploadedFile;
+use Illuminate\Support\Facades\Storage;
+use Tests\TestCase;
+
+class UserCanCreateDevicesWithoutColorsTest extends TestCase
+{
+    use RefreshDatabase;
+
+    final public function testUserCanCreateDevicesWithoutMarker()
+    {
+        Storage::fake('public');
+        $user = factory(User::class)->create();
+        $user->free = true;
+        $user->save();
+        $device = factory(Device::class)->make();
+        $response = $this->actingAs($user, 'api')->json('POST', '/api/devices', [
+            'name' => $device->name,
+            'is_public' => true,
+        ]);
+        $response->assertCreated();
+        $response->assertJsonStructure([
+            'name',
+            'description',
+            'user_uuid',
+            'is_public',
+            'uuid',
+            'updated_at',
+            'created_at',
+        ]);
+        $response->assertJsonFragment([
+            'name' => $device->name,
+            'is_public' => true,
+        ]);
+    }
+}
