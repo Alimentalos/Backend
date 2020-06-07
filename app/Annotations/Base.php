0ff11062a64062e33a44d38e4cb18e1e1a76e3b8
Ian Torres <iantorres@outlook.com>
2020-06-07 14:17:44 -0400

    Bump OAS version.

--- app/Annotations/Base.php
+++ app/Annotations/Base.php
@@ -2,7 +2,7 @@
 
 /**
  * @OA\Info(
- *      version="v.9.4",
+ *      version="v.9.6",
  *      title="Alimentalos API",
  *      description="Official Alimentalos API Documentation",
  *      @OA\Contact(
