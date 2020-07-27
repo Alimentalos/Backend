e96408100fccfd81947d5c8df23c7197a187310e
Daniel Duarte Erices <d.ignacio292@gmail.com>
2020-07-27 19:43:33 -0400

    Mostrar mascota añadido (Closes #190)

---
+++ resources/views/pets/show.blade.php
@@ -0,0 +1,104 @@
+@extends('layouts.app')
+@section('content')
+    <div class="">
+        <div class="flex-col justify-center">
+            <div class="px-4">
+                <div class="px-4 py-8 text-center">
+                    <h1 class="text-gray-500 text-4xl">Profile Dog</h1>
+                    <p class="text-gray-600 ">Lorem ipsum dolor sit amet, consectetur adipisicing elit</p>
+                </div>
+            </div>
+        </div>
+    </div>
+    <div class="mb-5">
+        <div class="bg-gray-200 w-full rounded-lg overflow-hidden container ml-auto mr-auto py-10 px-20">
+            <div class="bg-auto bg-center" style="background-image: url()">
+
+            </div>
+            <div class="px-20">
+                <div class="bg-white rounded-lg mb-5">
+                    <div class="flex justify-center">
+                        <div class="p-6">
+                            <h1 class="text-gray font-bold text-4xl">Firulais</h1>
+                            <p class="font-gray mb-3">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Aliquam aliquid assumenda at corporis cum doloremque eum expedita fugiat fugit id illum ipsum iste laborum, nemo neque provident, quisquam, ratione reiciendis.</p>
+                            <p class="font-gray text-1xl font-bold">200 Views</p>
+                        </div>
+                        <div class="py-6">
+                            <img class="w-64 rounded-lg" src="../img/background.jpg" alt="">
+                        </div>
+                    </div>
+                </div>
+                <div class="bg-white rounded-lg">
+                    <div class="flex justify-between py-6 px-20">
+                        <div class="text-center">
+                            <p class="text-gray">Type pet</p>
+                            <p class="font-gray font-bold">Dog</p>
+                        </div>
+                        <div class="text-center">
+                            <p class="text-gray">Sex</p>
+                            <p class="font-gray font-bold">Dog</p>
+                        </div>
+                        <div class="text-center">
+                            <p class="text-gray">Years</p>
+                            <p class="font-gray font-bold">Dog</p>
+                        </div>
+                    </div>
+                    <div class="flex justify-between py-6 px-20">
+                        <div class="text-center">
+                            <p class="text-gray">Type pet</p>
+                            <p class="font-gray font-bold">Dog</p>
+                        </div>
+                        <div class="text-center">
+                            <p class="text-gray">Type pet</p>
+                            <p class="font-gray font-bold">Dog</p>
+                        </div>
+                        <div class="text-center">
+                            <p class="text-gray">Type pet</p>
+                            <p class="font-gray font-bold">Dog</p>
+                        </div>
+                    </div>
+                    <div class="flex justify-between py-6 px-20">
+                        <div class="text-center">
+                            <p class="text-gray">Type pet</p>
+                            <p class="font-gray font-bold">Dog</p>
+                        </div>
+                        <div class="text-center">
+                            <p class="text-gray">Type pet</p>
+                            <p class="font-gray font-bold">Dog</p>
+                        </div>
+                        <div class="text-center">
+                            <p class="text-gray">Type pet</p>
+                            <p class="font-gray font-bold">Dog</p>
+                        </div>
+                    </div>
+                    <div class="container ml-auto mr-auto">
+                        <div class="flex justify-center py-6 px-20">
+                            <div class="">
+                                <i class="fas fa-paw" style="font-size: 4.3em; color:#27496D;"></i>
+                            </div>
+                            <div class="text-left px-20">
+                                <p class="text-gray">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ab assumenda blanditiis deserunt ea fugiat ipsam itaque iure.</p>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="flex justify-center py-10 space-x-4">
+                        <div class="">
+                            <button class="bg-blue-500 hover:bg-blue-700 text-white font-light py-2 px-4 rounded-full">
+                                Adopt pet
+                            </button>
+                        </div>
+                        <div class="">
+                            <button class="bg-blue-500 hover:bg-blue-700 text-white font-light py-2 px-4 rounded-full">
+                                Share pet
+                            </button>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+    <div class="">
+
+    </div>
+
+@endsection
