1d1a3139560bee9ebef3dfa6ed3c50cdfa82cf8f
Daniel Duarte Erices <d.ignacio292@gmail.com>
2020-07-27 12:57:58 -0400

    Formulario añadido en el archivo correcto.

---
+++ resources/views/pets/create.blade.php
@@ -0,0 +1,34 @@
+<form action="">
+    <div class="md:flex justify-center">
+        <div class="text-center px-2 mb-3">
+            <input class="bg-white focus:outline-none focus:shadow-outline border border-gray-300 rounded-lg py-2 px-4 block w-full appearance-none leading-normal" type="email" placeholder="Name">
+        </div>
+        <div class="text-center px-2 mb-3">
+            <input class="bg-white focus:outline-none focus:shadow-outline border border-gray-300 rounded-lg py-2 px-4 block w-full appearance-none leading-normal" type="email" placeholder="Hair color">
+        </div>
+    </div>
+    <div class="md:flex justify-center">
+        <div class="text-center px-2 mb-3">
+            <input class="bg-white focus:outline-none focus:shadow-outline border border-gray-300 rounded-lg py-2 px-4 block w-full appearance-none leading-normal" type="email" placeholder="Color eye left">
+        </div>
+        <div class="text-center px-2 mb-3">
+            <input class="bg-white focus:outline-none focus:shadow-outline border border-gray-300 rounded-lg py-2 px-4 block w-full appearance-none leading-normal" type="email" placeholder="Color eye right">
+        </div>
+    </div>
+    <div class="md:flex justify-center">
+        <div class="text-center px-2 mb-3">
+            <input class="bg-white focus:outline-none focus:shadow-outline border border-gray-300 rounded-lg py-2 px-4 block w-full appearance-none leading-normal" type="email" placeholder="Size">
+        </div>
+        <div class="text-center px-2 mb-3">
+            <input class="bg-white focus:outline-none focus:shadow-outline border border-gray-300 rounded-lg py-2 px-4 block w-full appearance-none leading-normal" type="email" placeholder="Location">
+        </div>
+    </div>
+    <div class="flex w-full px-2 mb-3">
+        <textarea class="resize border w-full rounded focus:outline-none focus:shadow-outline border border-gray-300 py-2 px-4" placeholder="Description..." rows="4"></textarea>
+    </div>
+    <div class="text-center mb-3 pt-4">
+        <button class="bg-blue-500 hover:bg-blue-700 text-white font-light py-2 px-4 rounded-full">
+            Save dog
+        </button>
+    </div>
+</form>
