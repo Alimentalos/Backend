db6f04c9d579d635cae100dc4e7c44f455682cb2
Daniel Duarte <d.ignacio292@gmail.com>
2020-07-27 12:36:32 -0400

    Revert "177 crear formulario mascotas"

--- resources/views/components/start.blade.php
+++ resources/views/components/start.blade.php
@@ -2,10 +2,10 @@
     <h1 class="text-white font-bold uppercase text-4xl">Lorem ipsum dolor sit amet</h1>
     <p class="text-white text-light">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ad adipisci amet consequatur cum.</p>
     <div class="flex justify-center py-10 space-x-4">
-        <div class="py-2">
-            <a href="#" class="bg-blue-500 hover:bg-blue-700 text-white font-light py-2 px-4 rounded-full">
-                Pet to adopt
-            </a>
+        <div class="">
+            <button class="bg-blue-500 hover:bg-blue-700 text-white font-light py-2 px-4 rounded-full">
+                Register pet
+            </button>
         </div>
         <div class="">
             <button class="bg-blue-500 hover:bg-blue-700 text-white font-light py-2 px-4 rounded-full">
