b1ebcc01fdf404041aea5535a2f1423d41437ada
Daniel Duarte <d.ignacio292@gmail.com>
2020-07-27 12:02:12 -0400

    Cambio revertido

--- resources/views/components/start.blade.php
+++ resources/views/components/start.blade.php
@@ -3,7 +3,7 @@
     <p class="text-white text-light">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ad adipisci amet consequatur cum.</p>
     <div class="flex justify-center py-10 space-x-4">
         <div class="py-2">
-            <a href="{{ route ('pet') }}" class="bg-blue-500 hover:bg-blue-700 text-white font-light py-2 px-4 rounded-full">
+            <a href="#" class="bg-blue-500 hover:bg-blue-700 text-white font-light py-2 px-4 rounded-full">
                 Pet to adopt
             </a>
         </div>
