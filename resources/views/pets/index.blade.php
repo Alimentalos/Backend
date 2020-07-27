d87edd7ac489f4b9a98cdd99bf968038a821a5f7
Daniel Duarte Erices <d.ignacio292@gmail.com>
2020-07-27 15:39:07 -0400

    index de mascotas añadido (Closes #185)

---
+++ resources/views/pets/index.blade.php
@@ -0,0 +1,13 @@
+<div class="bg-gray">
+    <a href="#">
+        <div class="flex">
+            <div class="">
+                <img src="img/background.jpg" alt="">
+            </div>
+            <div class="p-6">
+                <span class="text-white font-bold">Firulais</span>
+                <span class="text-gray font-light">Lorem ipsum dolor sit amet, consectetur adipisicing elit.</span>
+            </div>
+        </div>
+    </a>
+</div>
