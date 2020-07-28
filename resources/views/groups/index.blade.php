dcaa41a5ac00ac8711822c614f82e64c66066bea
Daniel Duarte Erices <d.ignacio292@gmail.com>
2020-07-28 16:43:32 -0400

    Index de grupos añadia (Closes #193)

---
+++ resources/views/groups/index.blade.php
@@ -0,0 +1,88 @@
+@extends('layouts.app')
+@section('content')
+    <div class="">
+        <div class="flex justify-center">
+            <div class="px-4">
+                <div class="px-4 py-8 text-center">
+                    <h1 class="text-gray-500 text-4xl">Index the group</h1>
+                    <p class="text-gray-600 ">Lorem ipsum dolor sit amet, consectetur adipisicing elit</p>
+                </div>
+            </div>
+        </div>
+        <div class="container ml-auto mr-auto">
+            <hr class="ml-auto mr-auto mb-5" style="width:70%;">
+            <div class="">
+                <div class="md:flex justify-center">
+                    <div class="px-6 sm:px-20 mb-3 pt-3">
+                        <img class="w-70 h-40 rounded-lg" src="../img/background.jpg" alt="">
+                    </div>
+                    <div class="px-6 mb-5">
+                        <article>
+                            <div class="text-left sm:text-left xl:text-left">
+                                <h2 class="text-teal-900 font-bold text-4xl">Name group</h2>
+                                <p class="text-teal-400 font-bold mb-2">Create 3 days ago</p>
+                                <p class="text-gray-700">
+                                    Lorem ipsum dolor sit amet, consectetur adipisicing elit. <br>Amet at atque blanditiis consectetur corporis.
+                                </p>
+                            </div>
+                        </article>
+                    </div>
+                    <div class="text-center px-6 mb-3 mt-5 pt-5">
+                        <button class="bg-blue-500 hover:bg-blue-700 text-white font-light py-2 px-4 rounded-full">
+                            Join the group
+                        </button>
+                    </div>
+                </div>
+            </div>
+            <hr class="ml-auto mr-auto mb-5" style="width:70%;">
+            <div class="">
+                <div class="md:flex justify-center">
+                    <div class="px-6 sm:px-20 mb-3 pt-3">
+                        <img class="w-70 h-40 rounded-lg" src="../img/background.jpg" alt="">
+                    </div>
+                    <div class="px-6 mb-5">
+                        <article>
+                            <div class="text-left sm:text-left xl:text-left">
+                                <h2 class="text-teal-900 font-bold text-4xl">Name group</h2>
+                                <p class="text-teal-400 font-bold mb-2">Create 3 days ago</p>
+                                <p class="text-gray-700">
+                                    Lorem ipsum dolor sit amet, consectetur adipisicing elit. <br>Amet at atque blanditiis consectetur corporis.
+                                </p>
+                            </div>
+                        </article>
+                    </div>
+                    <div class="text-center px-6 mb-3 mt-5 pt-5">
+                        <button class="bg-blue-500 hover:bg-blue-700 text-white font-light py-2 px-4 rounded-full">
+                            Join the group
+                        </button>
+                    </div>
+                </div>
+            </div>
+            <hr class="ml-auto mr-auto mb-5" style="width:70%;">
+            <div class="">
+                <div class="md:flex justify-center">
+                    <div class="px-6 sm:px-20 mb-3 pt-3">
+                        <img class="w-70 h-40 rounded-lg" src="../img/background.jpg" alt="">
+                    </div>
+                    <div class="px-6 mb-5">
+                        <article>
+                            <div class="text-left sm:text-left xl:text-left">
+                                <h2 class="text-teal-900 font-bold text-4xl">Name group</h2>
+                                <p class="text-teal-400 font-bold mb-2">Create 3 days ago</p>
+                                <p class="text-gray-700">
+                                    Lorem ipsum dolor sit amet, consectetur adipisicing elit. <br>Amet at atque blanditiis consectetur corporis.
+                                </p>
+                            </div>
+                        </article>
+                    </div>
+                    <div class="text-center px-6 mb-3 mt-5 pt-5">
+                        <button class="bg-blue-500 hover:bg-blue-700 text-white font-light py-2 px-4 rounded-full">
+                            Join the group
+                        </button>
+                    </div>
+                </div>
+            </div>
+            <hr class="ml-auto mr-auto mb-5" style="width:70%;">
+        </div>
+    </div>
+@endsection
