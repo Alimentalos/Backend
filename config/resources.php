fffbe37eddf6e1082fc7b387fae4fb3568f9109a
Ian Torres <iantorres@outlook.com>
2021-01-24 23:29:02 -0300

    Resource fixing.

--- config/resources.php
+++ config/resources.php
@@ -1,30 +1,30 @@
 <?php
 
 return [
-	/*
+    /*
     |--------------------------------------------------------------------------
     | Listable
     |--------------------------------------------------------------------------
     |
     */
-	'listable' => [
-		'places' => \App\Models\Place::class,
-		'users' => \App\Models\User::class,
-		'groups' => \App\Models\Group::class,
-		'geofences' => \App\Models\Geofence::class,
-		'pets' => \App\Models\Pet::class,
-		'devices' => \App\Models\Device::class,
-		'photos' => \App\Models\Photo::class,
-		'actions' => \App\Models\Action::class,
-		'alerts' => \App\Models\Alert::class
-	],
-	/*
+    'listable' => [
+        'places' => \App\Models\Place::class,
+        'users' => \App\Models\User::class,
+        'groups' => \App\Models\Group::class,
+        'geofences' => \App\Models\Geofence::class,
+        'pets' => \App\Models\Pet::class,
+        'devices' => \App\Models\Device::class,
+        'photos' => \App\Models\Photo::class,
+        'actions' => \App\Models\Action::class,
+        'alerts' => \App\Models\Alert::class
+    ],
+    /*
     |--------------------------------------------------------------------------
     | Storable
     |--------------------------------------------------------------------------
     |
     */
-	'storable' => [
+    'storable' => [
         'places' => \App\Models\Place::class,
         'users' => \App\Models\User::class,
         'pets' => \App\Models\Pet::class,
@@ -32,33 +32,33 @@ return [
         'geofences' => \App\Models\Geofence::class,
         'devices' => \App\Models\Device::class,
         'alerts' => \App\Models\Alert::class
-	],
-	/*
+    ],
+    /*
     |--------------------------------------------------------------------------
     | Viewable
     |--------------------------------------------------------------------------
     |
     */
-	'viewable' => [
-		'places' => \App\Models\Place::class,
-		'groups' => \App\Models\Group::class,
-		'locations' => \App\Models\Location::class,
-		'actions' => \App\Models\Action::class,
-		'geofences' => \App\Models\Geofence::class,
-		'pets' => \App\Models\Pet::class,
-		'devices' => \App\Models\Device::class,
-		'users' => \App\Models\User::class,
-		'photos' => \App\Models\Photo::class,
-		'comments' => \App\Models\Comment::class,
-		'alerts' => \App\Models\Alert::class
-	],
-	/*
+    'viewable' => [
+        'places' => \App\Models\Place::class,
+        'groups' => \App\Models\Group::class,
+        'locations' => \App\Models\Location::class,
+        'actions' => \App\Models\Action::class,
+        'geofences' => \App\Models\Geofence::class,
+        'pets' => \App\Models\Pet::class,
+        'devices' => \App\Models\Device::class,
+        'users' => \App\Models\User::class,
+        'photos' => \App\Models\Photo::class,
+        'comments' => \App\Models\Comment::class,
+        'alerts' => \App\Models\Alert::class
+    ],
+    /*
     |--------------------------------------------------------------------------
     | Modifiable
     |--------------------------------------------------------------------------
     |
     */
-	'modifiable' => [
+    'modifiable' => [
         'places' => \App\Models\Place::class,
         'alerts' => \App\Models\Alert::class,
         'comments' => \App\Models\Comment::class,
@@ -68,33 +68,33 @@ return [
         'pets' => \App\Models\Pet::class,
         'groups' => \App\Models\Group::class,
         'geofences' => \App\Models\Geofence::class,
-	],
-	/*
+    ],
+    /*
     |--------------------------------------------------------------------------
     | Removable
     |--------------------------------------------------------------------------
     |
     */
-	'removable' => [
-		'places' => \App\Models\Place::class,
-		'photos' => \App\Models\Photo::class,
-		'users' => \App\Models\User::class,
-		'comments' => \App\Models\Comment::class,
-		'actions' => \App\Models\Action::class,
-		'devices' => \App\Models\Device::class,
-		'pets' => \App\Models\Pet::class,
-		'geofences' => \App\Models\Geofence::class,
-		'groups' => \App\Models\Group::class,
-		'alerts' => \App\Models\Alert::class
-	],
-	/*
+    'removable' => [
+        'places' => \App\Models\Place::class,
+        'photos' => \App\Models\Photo::class,
+        'users' => \App\Models\User::class,
+        'comments' => \App\Models\Comment::class,
+        'actions' => \App\Models\Action::class,
+        'devices' => \App\Models\Device::class,
+        'pets' => \App\Models\Pet::class,
+        'geofences' => \App\Models\Geofence::class,
+        'groups' => \App\Models\Group::class,
+        'alerts' => \App\Models\Alert::class
+    ],
+    /*
     |--------------------------------------------------------------------------
     | Tokenized
     |--------------------------------------------------------------------------
     |
     */
-	'tokenized' => [
+    'tokenized' => [
         'pets' => \App\Models\Pet::class,
         'devices' => \App\Models\Device::class,
-	],
+    ],
 ];
